97553accc0e42bd2466fec712e71f284ac285946
Shannon Whalen <shannon@shannonwhalen.com>
2017-03-01 20:07:36 -0500

    final ottergram project

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,11 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
-var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]'
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_FRAME_SELECTOR='[data-image-role="frame"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS='is-tiny';
+var ESC_KEY = 27;
+
 
 function setDetails(imageUrl, titleText){
 
@@ -32,6 +37,7 @@ function addThumbClickHandler(thumb){
   thumb.addEventListener('click', function(event){
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetails();
   });
 }
 
@@ -42,10 +48,34 @@ function getThumbnailsArray(){
   return thumbnailsArray;
 }
 
+function hideDetails(){
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler(){
+  document.body.addEventListener('keyup', function(event){
+    event.preventDefault();
+    console.log(event.keyCode);
+
+    if(event.keyCode === 27){
+      hideDetails();
+    }
+  });
+}
+
+function showDetails(){
+  var frame=document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function(){
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
+}
 function initializeEvents(){
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
